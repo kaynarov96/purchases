b884ae4d3d92e44be88156af9d03c59f711d4af6
kaynarov96 <student@localhost>
2020-01-28 12:56:32 +0500

    Add Project

--- src/js/app.js
+++ src/js/app.js
@@ -9,12 +9,17 @@ rootEl.innerHTML = `
     <button data-action="purchase-add">Добавить</button>
 </form>
 <div data-id="purchase-total">0</div>
+
+<ul data-id="purchases-list"></ul>
+<div data-id="purchases-total"></div>
 `;
 const purchaseAddFormEl = rootEl.querySelector('data-id=purchase-add-form');
 
 const purchaseInputAmountEl = purchaseAddFormEl.querySelector('[data-id=purchase-input-amount]');
 const purchaseInputCategoryEl = purchaseAddFormEl.querySelector('[data-id=purchase-input-category]');
 const purchaseAddButtonEl = purchaseAddFormEl.querySelector('[data-id=purchase-add]');
+
+const purchasesListEl = rootEl.querySelector('[data-action=purchase-add]');
 
 let purchasesTotal = 0;
 
@@ -23,9 +28,20 @@ purchaseAddButtonEl.onclick = evt => {
 
     const value = purchaseInputAmountEl.value;
     purchasesTotal +=parsentInt(value, 10);
+
+    const category = purchaseInputCAtegoryEl.value
+
     purchasesTotalEl.textContent = `Сумма: ${purchasesTotal}`;
 
+    const purchaseEl = document.createElement('li');
+    purchaseEl.innerHTML = `
+    Покупка на сумму ${value}, в категории ${category}
+    `;
+    purchasesListEl.insertBefore(purchaseEl, purchasesListEl.firstElementChild)
     purchaseInputAmountEl.value = '';
+    purchaseInputCategory.value = '';
+
+    purchaseInputAmountEl(focus);
 };
 
 const pusrchasesTotal = rootEl.querySelector(rootEl);
